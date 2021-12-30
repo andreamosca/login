319acb7fc0ff40a6a9cd5e81f079228238f4175a
andreamosca <andrea.mosca.prod@gmail.com>
2021-12-30 19:26:42 +0100

    utilizzo di useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -10,12 +10,21 @@ function App() {
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem("login", "1");
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem("login");
     setIsLoggedIn(false);
   };
+
+  useEffect(() => {
+    const loginInfo = localStorage.getItem("login"); 
+    if(loginInfo === "1"){
+      setIsLoggedIn(true);
+    }
+  },[/*app start*/])
 
   return (
     <React.Fragment>

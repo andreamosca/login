2fc8db2558a7c85867d3ba540577104b3557778a
andreamosca <andrea.mosca.prod@gmail.com>
2021-12-31 12:48:05 +0100

    funzione di cleanup dell'Effect (Debouncing)

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -12,9 +12,16 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes("@") && enteredPassword.trim().length > 6
-    );
+    const timer = setTimeout(() => {
+      console.log("check!!");
+      setFormIsValid(
+        enteredEmail.includes("@") && enteredPassword.trim().length > 6
+      );
+    }, 500);
+    return () => {
+      console.log("CLEAR");
+      clearTimeout(timer);
+    }
   }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {

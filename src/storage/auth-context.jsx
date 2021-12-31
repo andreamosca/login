ed3a9373a773210699254683af8e92ed12f206b8
andreamosca <andrea.mosca.prod@gmail.com>
2021-12-31 17:24:09 +0100

    creazione di un Context component che centralizza la gestione del login

--- src/storage/auth-context.jsx
+++ src/storage/auth-context.jsx
@@ -1,8 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const AuthContext = React.createContext({
   isLoggedIn: false,
-  onLogout: () => {}
+  onLogout: () => {},
+  onLogin: () => {},
 });
+
+export const AuthContextProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    localStorage.setItem("login", "1");
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    localStorage.removeItem("login");
+    setIsLoggedIn(false);
+  };
+
+  useEffect(() => {
+      const loginInfo = localStorage.getItem("login");
+      if (loginInfo === "1") {
+        setIsLoggedIn(true);
+      }
+    }, [/*app start*/]);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+        onLogin: loginHandler,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
 
 export default AuthContext;

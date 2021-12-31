8a8da596cd49181e9f0765588b6657a2e121a290
andreamosca <andrea.mosca.prod@gmail.com>
2021-12-31 17:24:28 +0100

    implementazione di AuthContextProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 
 import AuthContext from './storage/auth-context';
 
@@ -7,38 +7,16 @@ import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
-    localStorage.setItem("login", "1");
-    setIsLoggedIn(true);
-  };
-
-  const logoutHandler = () => {
-    localStorage.removeItem("login");
-    setIsLoggedIn(false);
-  };
-
-  useEffect(() => {
-    const loginInfo = localStorage.getItem("login"); 
-    if(loginInfo === "1"){
-      setIsLoggedIn(true);
-    }
-  },[/*app start*/])
+  const AuthCtx = useContext(AuthContext);
 
   return (
-    <AuthContext.Provider value={{
-      isLoggedIn: isLoggedIn,
-      onLogout: logoutHandler
-    }}>
+    <React.Fragment>
       <MainHeader />
       <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home/>}
+        {!AuthCtx.isLoggedIn && <Login/>}
+        {AuthCtx.isLoggedIn && <Home/>}
       </main>
-    </AuthContext.Provider>
+    </React.Fragment>
   );
 }
 
